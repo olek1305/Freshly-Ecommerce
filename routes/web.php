28516d5ad1e154f69bfa6976b244559f4dbfa2e5
Aleksander Żak <olek1305@gmail.com>
2024-05-16 20:52:11 +0200

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ImageController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/home', HomeController::class)->name('home');
-
-Route::post('/upload-file', [ImageController::class, 'handleImage'])->name('upload-file');
-
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'handleLogin'])->name('login.submit');
