d87a77bc3e79f644e4e10b984aa3332a4b47112b
Aleksander Żak <olek1305@gmail.com>
2024-05-31 14:48:39 +0200

    installation and setup paypal, remove unnecessary files

---
+++ app/Http/Controllers/Gateways/PaypalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Gateways;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaypalController extends Controller
+{
+    public function payment()
+    {
+
+    }
+
+    public function success()
+    {
+
+    }
+
+    public function cancel()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
-use App\DataTables\UsersDataTable;
-use App\Http\Controllers\CartController;
+use App\Http\Controllers\Gateways\PaypalController;
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Imagick\Driver;
 use Laravel\Socialite\Facades\Socialite;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,29 +23,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('user/{id}/edit', function ($id) {
-   return $id;
-})->name('user.edit');
-
-Route::get('/dashboard', function (UsersDataTable $dataTable) {
-    return $dataTable->render('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/image', function () {
-    $manager = new ImageManager(new Driver());
-    $image = $manager->read('car.jpg');
-    $image->resize(1001, 1001);
-    $image = $image->blur(20);
-    $image->save('car.jpg');
-});
-
-Route::get('shop', [CartController::class, 'shop'])->name('shop');
-Route::get('cart', [CartController::class, 'cart'])->name('cart');
-Route::get('add-to-cart/{product_id}', [CartController::class, 'addToCart'])->name('addToCart');
-
-Route::get('qty-increment/{rowId}', [CartController::class, 'qtyIncrement'])->name('qtyIncrement');
-Route::get('qty-decrement/{rowId}', [CartController::class, 'qtyDecrement'])->name('qtyDecrement');
-Route::get('remove-product/{rowId}', [CartController::class, 'removeProduct'])->name('removeProduct');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -58,28 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('create-role', function() {
-//    $role = Role::create(['name' => 'publisher']);
-//
-//    return $role;
-
-//    $permission = Permission::create(['name' => 'edit articles']);
-//
-//    return $permission;
-
-    $user = auth()->user();
-//    $user->assignRole('writer');
-//    $user->givePermissionTo('edit articles');
-
-
-
-    if($user->can('edit articles')) {
-        return 'user have permission';
-    } else {
-        return 'user dont have permission';
-    }
-
-});
+require __DIR__.'/auth.php';
 
 Route::get('/auth/redirect', function() {
     return Socialite::driver('github')->redirect();
@@ -100,4 +54,7 @@ Route::get('/auth/callback', function() {
     return redirect('/dashboard');
 });
 
-require __DIR__.'/auth.php';
+Route::post('paypal/payment', [PaypalController::class, 'payment'])->name('paypal.payment');
+Route::get('paypal/success', [PaypalController::class, 'success'])->name('paypal.success');
+Route::get('paypal/cancel', [PaypalController::class, 'cancel'])->name('paypal.cancel');
+
