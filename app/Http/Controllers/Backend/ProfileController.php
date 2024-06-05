b80e30dadbd9978398897b70dc53cb2ea3a92a2f
Aleksander Żak <olek1305@gmail.com>
2024-06-05 10:44:17 +0200

    add change data for admin

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
 
 class ProfileController extends Controller
 {
@@ -18,12 +20,41 @@ class ProfileController extends Controller
         $request->validate([
             'name' => 'required|max:100',
             'email' => 'required|email|unique:users,email,'.Auth::id(),
+            'image' => 'mimes:jpeg,jpg,png|max:2048'
         ]);
 
         $user = Auth::user();
+
+        if($request->hasFile('image')) {
+            if(File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+            $image = $request->image;
+            $imageName = rand() . '_' . $image->getClientOriginalExtension();
+            $image->move(public_path('uploads'), $imageName);
+
+            $path = '/uploads/' . $imageName;
+
+            $user->image = $path;
+        }
+
         $user->name = $request->name;
         $user->email = $request->email;
         $user->save();
+
+        return redirect()->back();
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => 'required|current_password',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $request->user()->update([
+           'password' => bcrypt($request->password)
+        ]);
 
         return redirect()->back();
     }
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -13,13 +13,21 @@
             <div class="row mt-sm-4">
                 <div class="col-12 col-md-12 col-lg-7">
                     <div class="card">
-                        <form method="post" class="needs-validation" novalidate="" action="{{ route('admin.profile.update') }}">
+                        <form method="post" class="needs-validation" novalidate="" action="{{ route('admin.profile.update') }}"
+                            enctype="multipart/form-data">
                             @csrf
                             <div class="card-header">
                                 <h4>Update Profile</h4>
                             </div>
                             <div class="card-body">
                                 <div class="row">
+                                    <div class="form-group col-12">
+                                        <div class="mb-3">
+                                            <img width="100px" src="{{ asset(Auth::user()->image) }}" alt="photo"/>
+                                        </div>
+                                        <label>Image</label>
+                                        <input type="file" class="form-control" name="image">
+                                    </div>
                                     <div class="form-group col-md-6 col-12">
                                         <label>Name</label>
                                         <input type="text" class="form-control" name="name" value="{{ Auth::user()->name }}" required="">
@@ -27,6 +35,42 @@
                                     <div class="form-group col-md-6 col-12">
                                         <label>Email</label>
                                         <input type="text" class="form-control" name="email" value="{{ Auth::user()->email }}" required="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer text-right">
+                                <button class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-12 col-lg-7">
+                    <div class="card">
+                        @if($errors->any())
+                            @foreach($errors->all() as $error)
+                                <span class="alert alert-danger">{{ $error }}</span>
+                            @endforeach
+                        @endif
+                        <form method="post" class="needs-validation" novalidate="" action="{{ route('admin.password.update') }}"
+                              enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-header">
+                                <h4>Update Password</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group col-md-6 col-12">
+                                        <label>Current Password</label>
+                                        <input type="password" class="form-control" name="current_password">
+                                    </div>
+                                    <div class="form-group col-md-6 col-12">
+                                        <label>New Password</label>
+                                        <input type="password" class="form-control" name="password">
+                                    </div>
+                                    <div class="form-group col-md-6 col-12">
+                                        <label>Confirm Password</label>
+                                        <input type="password" class="form-control" name="password_confirmation">
                                     </div>
                                 </div>
                             </div>
