3469a827802e80f954d5fb61538d963bb0593d72
Aleksander Żak <olek1305@gmail.com>
2024-06-08 18:40:28 +0200

    fix bug and spaces

--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -30,77 +30,78 @@
 <body>
 
 
-<!--=============================
-  DASHBOARD MENU START
-==============================-->
-<div class="wsus__dashboard_menu">
-    <div class="wsusd__dashboard_user">
-        <img src="images/dashboard_user.jpg" alt="img" class="img-fluid">
-        <p>anik roy</p>
+    <!--=============================
+      DASHBOARD MENU START
+    ==============================-->
+    <div class="wsus__dashboard_menu">
+        <div class="wsusd__dashboard_user">
+            <img src="images/dashboard_user.jpg" alt="img" class="img-fluid">
+            <p>anik roy</p>
+        </div>
     </div>
-</div>
-<!--=============================
-  DASHBOARD MENU END
-==============================-->
+    <!--=============================
+      DASHBOARD MENU END
+    ==============================-->
 
 
-<!--=============================
-  DASHBOARD START
-==============================-->
-@yield('content')
-<!--=============================
-  DASHBOARD START
-==============================-->
+    <!--=============================
+      DASHBOARD START
+    ==============================-->
+    @yield('content')
+    <!--=============================
+      DASHBOARD START
+    ==============================-->
 
 
-<!--============================
-    SCROLL BUTTON START
-  ==============================-->
-<div class="wsus__scroll_btn">
-    <i class="fas fa-chevron-up"></i>
-</div>
-<!--============================
-  SCROLL BUTTON  END
-==============================-->
+    <!--============================
+        SCROLL BUTTON START
+      ==============================-->
+    <div class="wsus__scroll_btn">
+        <i class="fas fa-chevron-up"></i>
+    </div>
+    <!--============================
+      SCROLL BUTTON  END
+    ==============================-->
 
 
-<!--jquery library js-->
-<script src="{{ asset('frontend/assets/js/jquery-3.6.0.min.js')}}"></script>
-<!--bootstrap js-->
-<script src="{{ asset('frontend/assets/js/bootstrap.bundle.min.js')}}"></script>
-<!--font-awesome js-->
-<script src="{{ asset('frontend/assets/js/Font-Awesome.js')}}"></script>
-<!--select2 js-->
-<script src="{{ asset('frontend/assets/js/select2.min.js')}}"></script>
-<!--slick slider js-->
-<script src="{{ asset('frontend/assets/js/slick.min.js')}}"></script>
-<!--simplyCountdown js-->
-<script src="{{ asset('frontend/assets/js/simplyCountdown.js')}}"></script>
-<!--product zoomer js-->
-<script src="{{ asset('frontend/assets/js/jquery.exzoom.js')}}"></script>
-<!--nice-number js-->
-<script src="{{ asset('frontend/assets/js/jquery.nice-number.min.js')}}"></script>
-<!--counter js-->
-<script src="{{ asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
-<script src="{{ asset('frontend/assets/js/jquery.countup.min.js')}}"></script>
-<!--add row js-->
-<script src="{{ asset('frontend/assets/js/add_row_custon.js')}}"></script>
-<!--multiple-image-video js-->
-<script src="{{ asset('frontend/assets/js/multiple-image-video.js')}}"></script>
-<!--sticky sidebar js-->
-<script src="{{ asset('frontend/assets/js/sticky_sidebar.js')}}"></script>
-<!--price ranger js-->
-<script src="{{ asset('frontend/assets/js/ranger_jquery-ui.min.js')}}"></script>
-<script src="{{ asset('frontend/assets/js/ranger_slider.js')}}"></script>
-<!--isotope js-->
-<script src="{{ asset('frontend/assets/js/isotope.pkgd.min.js')}}"></script>
-<!--venobox js-->
-<script src="{{ asset('frontend/assets/js/venobox.min.js')}}"></script>
-<!--classycountdown js-->
-<script src="{{ asset('frontend/assets/js/jquery.classycountdown.js')}}"></script>
+    <!--jquery library js-->
+    <script src="{{ asset('frontend/assets/js/jquery-3.6.0.min.js')}}"></script>
+    <!--bootstrap js-->
+    <script src="{{ asset('frontend/assets/js/bootstrap.bundle.min.js')}}"></script>
+    <!--font-awesome js-->
+    <script src="{{ asset('frontend/assets/js/Font-Awesome.js')}}"></script>
+    <!--select2 js-->
+    <script src="{{ asset('frontend/assets/js/select2.min.js')}}"></script>
+    <!--slick slider js-->
+    <script src="{{ asset('frontend/assets/js/slick.min.js')}}"></script>
+    <!--simplyCountdown js-->
+    <script src="{{ asset('frontend/assets/js/simplyCountdown.js')}}"></script>
+    <!--product zoomer js-->
+    <script src="{{ asset('frontend/assets/js/jquery.exzoom.js')}}"></script>
+    <!--nice-number js-->
+    <script src="{{ asset('frontend/assets/js/jquery.nice-number.min.js')}}"></script>
+    <!--counter js-->
+    <script src="{{ asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery.countup.min.js')}}"></script>
+    <!--add row js-->
+    <script src="{{ asset('frontend/assets/js/add_row_custon.js')}}"></script>
+    <!--multiple-image-video js-->
+    <script src="{{ asset('frontend/assets/js/multiple-image-video.js')}}"></script>
+    <!--sticky sidebar js-->
+    <script src="{{ asset('frontend/assets/js/sticky_sidebar.js')}}"></script>
+    <!--price ranger js-->
+    <script src="{{ asset('frontend/assets/js/ranger_jquery-ui.min.js')}}"></script>
+    <script src="{{ asset('frontend/assets/js/ranger_slider.js')}}"></script>
+    <!--isotope js-->
+    <script src="{{ asset('frontend/assets/js/isotope.pkgd.min.js')}}"></script>
+    <!--venobox js-->
+    <script src="{{ asset('frontend/assets/js/venobox.min.js')}}"></script>
+    <!--classycountdown js-->
+    <script src="{{ asset('frontend/assets/js/jquery.classycountdown.js')}}"></script>
 
-<!--main/custom js-->
-<script src="{{ asset('frontend/assets/js/main.js')}}"></script>
+    <!--main/custom js-->
+    <script src="{{ asset('frontend/assets/js/main.js')}}"></script>
+
 </body>
 
 </html>
