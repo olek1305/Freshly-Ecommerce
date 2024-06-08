5cd5ff45729ff6ae430b30618e7b00d356c74340
Aleksander Żak <olek1305@gmail.com>
2024-06-08 18:45:06 +0200

    add validate, route and update for profile-user

--- app/Http/Controllers/Frontend/UserProfileController.php
+++ app/Http/Controllers/Frontend/UserProfileController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class UserProfileController extends Controller
 {
     public function index()
     {
         return view('frontend.dashboard.profile');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:100',
+            'email' => 'required|email|unique:users,email,'.Auth::id(),
+            'image' => 'mimes:jpeg,jpg,png|max:2048'
+        ]);
+
+        $user = Auth::user();
+
+        if($request->hasFile('image')) {
+            if(File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+            $image = $request->image;
+            $imageName = rand() . '_' . $image->getClientOriginalExtension();
+            $image->move(public_path('uploads'), $imageName);
+
+            $path = '/uploads/' . $imageName;
+
+            $user->image = $path;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        flash('Profile updated successfully!');
+
+        return redirect()->back();
     }
 }
--- resources/views/frontend/dashboard/profile.blade.php
+++ resources/views/frontend/dashboard/profile.blade.php
@@ -14,99 +14,58 @@
                         <div class="wsus__dashboard_profile">
                             <div class="wsus__dash_pro_area">
                                 <h4>basic information</h4>
-                                <form>
-                                    <div class="row">
-                                        <div class="col-xl-9">
-                                            <div class="row">
-                                                <div class="col-xl-6 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fas fa-user-tie"></i>
-                                                        <input type="text" placeholder="First Name">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fas fa-user-tie"></i>
-                                                        <input type="text" placeholder="Last Name">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="far fa-phone-alt"></i>
-                                                        <input type="text" placeholder="Phone">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-6 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fal fa-envelope-open"></i>
-                                                        <input type="email" placeholder="Email">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-12">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <textarea cols="3" rows="5" placeholder="About You"></textarea>
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-12">
-                                                    <div id="medicine_row3">
-                                                        <div class="row">
-                                                            <div class="col-xl-5 col-md-5">
-                                                                <div class="medicine_row_input">
-                                                                    <input type="text" placeholder="www.facebook.com" name="name[]">
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-xl-5 col-md-5">
-                                                                <div class="medicine_row_input">
-                                                                    <input type="text" placeholder="www.youtube.com" name="name[]">
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-xl-2 col-md-2">
-                                                                <div class="medicine_row_input">
-                                                                    <button type="button" id="add_row3"><i class="fas fa-plus"
-                                                                                                           aria-hidden="true"></i></button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                                    <form action="{{ route('user.profile.update') }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="col-md-12">
+                                            <div class="col-xl-2 col-sm-5 col-md-2 mb-5">
+                                                <div class="wsus__dash_pro_img">
+                                                    <img src="{{ Auth::user()->image ? asset(Auth::user()->image) : asset('frontend/assets/images/ts-2.jpg') }}" alt="photo" class="img-fluid w-100">
+                                                    <input type="file" name="image">
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-xl-3 col-sm-6 col-md-6">
-                                            <div class="wsus__dash_pro_img">
-                                                <img src="images/ts-2.jpg" alt="img" class="img-fluid w-100">
-                                                <input type="file">
+                                            <div class="col-md-12">
+                                                <div class="wsus__dash_pro_single">
+                                                    <i class="fas fa-user-tie"></i>
+                                                    <input type="text" placeholder="Name" name="name" value="{{ Auth::user()->name }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="wsus__dash_pro_single">
+                                                    <i class="fal fa-envelope-open"></i>
+                                                    <input type="email" placeholder="Email" name="email" value="{{ Auth::user()->email }}">
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="col-xl-12">
-                                            <button class="common_btn mb-4 mt-2" type="submit">upload</button>
+                                            <button class="common_btn mb-4 mt-2" type="submit">update</button>
                                         </div>
-                                        <div class="wsus__dash_pass_change mt-2">
-                                            <div class="row">
-                                                <div class="col-xl-4 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fas fa-unlock-alt"></i>
-                                                        <input type="password" placeholder="Current Password">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-4 col-md-6">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fas fa-lock-alt"></i>
-                                                        <input type="password" placeholder="New Password">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-4">
-                                                    <div class="wsus__dash_pro_single">
-                                                        <i class="fas fa-lock-alt"></i>
-                                                        <input type="password" placeholder="Confirm Password">
-                                                    </div>
-                                                </div>
-                                                <div class="col-xl-12">
-                                                    <button class="common_btn" type="submit">upload</button>
-                                                </div>
+                                    </form>
+                                <div class="wsus__dash_pass_change mt-2">
+                                    <div class="row">
+                                        <div class="col-xl-4 col-md-6">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-unlock-alt"></i>
+                                                <input type="password" placeholder="Current Password">
                                             </div>
                                         </div>
+                                        <div class="col-xl-4 col-md-6">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-lock-alt"></i>
+                                                <input type="password" placeholder="New Password">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-4">
+                                            <div class="wsus__dash_pro_single">
+                                                <i class="fas fa-lock-alt"></i>
+                                                <input type="password" placeholder="Confirm Password">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-12">
+                                            <button class="common_btn" type="submit">upload</button>
+                                        </div>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
