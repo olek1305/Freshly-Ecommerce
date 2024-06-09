38a0800c169d57067f012567c50c4b9f50958e41
Aleksander Żak <olek1305@gmail.com>
2024-06-09 18:48:06 +0200

    add profile page with validation and update account

---
+++ app/Http/Controllers/Backend/VendorProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
+
+class VendorProfileController extends Controller
+{
+    public function index()
+    {
+        return view('vendor.dashboard.profile');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:100',
+            'email' => 'required|email|unique:users,email,'.Auth::id(),
+            'image' => 'mimes:jpeg,jpg,png|max:2048'
+        ]);
+
+        $user = Auth::user();
+
+        if($request->hasFile('image')) {
+            if(File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+            $image = $request->image;
+            $imageName = rand() . '_' . $image->getClientOriginalExtension();
+            $image->move(public_path('uploads'), $imageName);
+
+            $path = '/uploads/' . $imageName;
+
+            $user->image = $path;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        flash('Profile updated successfully!');
+
+        return redirect()->back();
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => 'required|current_password',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $request->user()->update([
+           'password' => bcrypt($request->password)
+        ]);
+
+        flash('Password updated successfully!');
+
+        return redirect()->back();
+    }
+}
--- routes/vendor.php
+++ routes/vendor.php
@@ -2,7 +2,13 @@
 
 
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorProfileController;
 use Illuminate\Support\Facades\Route;
 
 /** Vendor Routes */
 Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
+
+/** Profile Routes */
+Route::get('profile', [VendorProfileController::class, 'index'])->name('profile');
+Route::put('profile/update', [VendorProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [VendorProfileController::class, 'updatePassword'])->name('password.update');
