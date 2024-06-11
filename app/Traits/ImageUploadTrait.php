5c53528ee819f9132d31332117a4709099f6ffed
Aleksander Żak <olek1305@gmail.com>
2024-06-11 17:05:22 +0200

    add trait imageUpload

--- app/Traits/ImageUploadTrait.php
+++ app/Traits/ImageUploadTrait.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 trait ImageUploadTrait {
 
@@ -18,5 +19,31 @@ trait ImageUploadTrait {
 
             return $path . '/' . $imageName;
         }
+    }
+
+    public function updateImage(Request $request, $inputName, $path, $oldPath = null)
+    {
+        if($request->hasFile($inputName)) {
+            if(File::exists(public_path($oldPath))) {
+                File::delete(public_path($oldPath));
+            }
+
+            $image = $request->{$inputName};
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_' . uniqid() . '.' . $ext;
+
+            $image->move(public_path($path), $imageName);
+
+            return $path . '/' . $imageName;
+        }
+    }
+
+    public function deleteImage(string $path)
+    {
+        if(File::exists(public_path($path))) {
+            File::delete(public_path($path));
+        }
+
+        return true;
     }
 }
