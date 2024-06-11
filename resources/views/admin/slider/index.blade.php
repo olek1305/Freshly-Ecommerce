a048b371c76e15e33a5ae599a7edd9a1e18c8ac2
Aleksander Żak <olek1305@gmail.com>
2024-06-11 21:54:41 +0200

    add icon and change name title

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -4,11 +4,11 @@
     <!-- Main Content -->
     <section class="section">
         <div class="section-header">
-            <h1>Table</h1>
+            <h1>Slider</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Components</a></div>
-                <div class="breadcrumb-item">Table</div>
+                <div class="breadcrumb-item"></div>
             </div>
         </div>
 
@@ -19,7 +19,7 @@
                         <div class="card-header">
                             <h4>Simple Table</h4>
                             <div class="card-header-action">
-                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary">+ Create New</a>
+                                <a href="{{ route('admin.slider.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Create New</a>
                             </div>
                         </div>
                         <div class="card-body">
