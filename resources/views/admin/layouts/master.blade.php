980eefd2f0cb9513ae50cf3d8fe92351e23b1871
Aleksander Żak <olek1305@gmail.com>
2024-06-13 11:32:29 +0200

    remove GA

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -27,16 +27,6 @@
         <!-- Template CSS -->
         <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
-        <!-- Start GA -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-94034622-3');
-        </script>
-        <!-- /END GA -->
     </head>
     <body>
     <div id="app">
