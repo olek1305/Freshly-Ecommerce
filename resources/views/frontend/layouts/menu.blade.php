6273fa74e1df6b0571c310252299655bd6d724d3
Aleksander Żak <olek1305@gmail.com>
2024-06-17 15:13:47 +0200

    show categories with status 1

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -1,5 +1,12 @@
 @php
-    $categories = \App\Models\Category::all();
+    $categories = \App\Models\Category::where('status', 1)
+    ->with(['subCategories' => function($query) {
+        $query->where('status', 1)
+        ->with(['childCategories' => function($query) {
+            $query->where('status', 1);
+        }]);
+    }])
+    ->get();
 @endphp
 
 <nav class="wsus__main_menu d-none d-lg-block">
