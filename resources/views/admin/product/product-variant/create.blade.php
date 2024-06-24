84512e022bc75f654451bb74bd02be366a25d2f4
Aleksander Żak <olek1305@gmail.com>
2024-06-24 23:04:21 +0200

    added Product Variant and Product Variant Item with CRUDs

---
+++ resources/views/admin/product/product-variant/create.blade.php
@@ -0,0 +1,42 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!-- Main Content -->
+    <section class="section">
+        <div class="section-header">
+            <h1>Product Variant</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Create variant</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.products-variant.store') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" class="form-control" name="product" value="{{ request()->product }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputStatus">Status</label>
+                                    <select id="inputStatus" class="form-control" name="status" >
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
