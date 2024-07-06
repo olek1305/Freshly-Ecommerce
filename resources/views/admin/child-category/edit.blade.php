0b7fa0917a6099e641e6832cd6203393037bcc8a
Aleksander Żak <olek1305@gmail.com>
2024-07-06 18:43:33 +0200

    Code cleanup: blanks and spaces

--- resources/views/admin/child-category/edit.blade.php
+++ resources/views/admin/child-category/edit.blade.php
@@ -6,18 +6,15 @@
         <div class="section-header">
             <h1>Sub Category</h1>
         </div>
-
         <div class="section-body">
-
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
                             <h4>Update Child Category</h4>
-
                         </div>
                         <div class="card-body">
-                            <form action="{{route('admin.child-category.update', $childCategory->id)}}" method="POST">
+                            <form action="{{ route('admin.child-category.update', $childCategory->id) }}" method="POST">
                                 @csrf
                                 @method('PUT')
                                 <div class="form-group">
@@ -25,41 +22,41 @@
                                     <select id="inputState" class="form-control main-category" name="category_id">
                                         <option value="">Select</option>
                                         @foreach ($categories as $category)
-                                            <option {{$category->id == $childCategory->category_id ? 'selected' : ''}} value="{{$category->id}}">{{$category->name}}</option>
+                                            <option {{ $category->id === $childCategory->category_id ? 'selected' : '' }} value="{{ $category->id }}">{{ $category->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
+
                                 <div class="form-group">
                                     <label for="inputState">Sub Category</label>
                                     <select id="inputState" class="form-control sub-category" name="sub_category_id">
                                         <option value="">Select</option>
                                         @foreach ($subCategories as $subCategory)
-                                            <option {{$subCategory->id == $childCategory->sub_category_id ? 'selected' : ''}} value="{{$subCategory->id}}">{{$subCategory->name}}</option>
+                                            <option {{ $subCategory->id === $childCategory->sub_category_id ? 'selected' : '' }} value="{{ $subCategory->id }}">{{ $subCategory->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
+
                                 <div class="form-group">
                                     <label>Name</label>
-                                    <input type="text" class="form-control" name="name" value="{{$childCategory->name}}">
+                                    <input type="text" class="form-control" name="name" value="{{ $childCategory->name }}">
                                 </div>
+
                                 <div class="form-group">
                                     <label for="inputState">Status</label>
                                     <select id="inputState" class="form-control" name="status">
-                                        <option {{$childCategory->status === 1 ? 'selected' : ''}} value="1">Active</option>
-                                        <option {{$childCategory->status === 0 ? 'selected' : ''}} value="0">Inactive</option>
+                                        <option {{ $childCategory->status === 1 ? 'selected' : '' }} value="1">Active</option>
+                                        <option {{ $childCategory->status === 0 ? 'selected' : '' }} value="0">Inactive</option>
                                     </select>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Update</button>
                             </form>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         </div>
     </section>
-
 @endsection
 
 @push('scripts')
