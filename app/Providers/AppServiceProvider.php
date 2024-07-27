ede6b472cb1b3b9f2d7ff93550c79792bcbff003
Aleksander Żak <olek1305@gmail.com>
2024-07-27 14:16:36 +0200

    Add 404 error handling for missing 'general_settings' table and records

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Models\GeneralSetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,14 +27,23 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
+        /** check if it has table general_settings */
+        if (!Schema::hasTable('general_settings')) {
+            abort(404, 'Table general_settings not found.');
+        }
+
         $generalSetting = GeneralSetting::first();
 
-        /** set time zone */
-        Config::set('app.timezone', $generalSetting->timezone);
+        if ($generalSetting) {
+            /** set time zone */
+            Config::set('app.timezone', $generalSetting->timezone);
 
-        /** share variable at all view */
-        View::composer('*', function($view) use ($generalSetting) {
-            $view->with('settings', $generalSetting);
-        });
+            /** share variable at all view */
+            View::composer('*', function ($view) use ($generalSetting) {
+                $view->with('settings', $generalSetting);
+            });
+        } else {
+            abort(404, 'No general settings found.');
+        }
     }
 }
