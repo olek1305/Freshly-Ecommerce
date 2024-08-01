a76413422b8afbf93c8f13958e92530286fd0b7a
Aleksander Żak <olek1305@gmail.com>
2024-08-01 18:23:23 +0200

    change order by to ASC

--- app/Http/Controllers/Frontend/FlashSaleController.php
+++ app/Http/Controllers/Frontend/FlashSaleController.php
@@ -5,15 +5,13 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\FlashSale;
 use App\Models\FlashSaleItem;
-use App\Models\Product;
-use Illuminate\Http\Request;
 
 class FlashSaleController extends Controller
 {
     public function index()
     {
         $flashSaleDate = FlashSale::first();
-        $flashSaleItems = FlashSaleItem::where('status', 1)->orderBy('id', 'DESC')->paginate(20);
+        $flashSaleItems = FlashSaleItem::where('status', 1)->orderBy('id', 'ASC')->paginate(20);
 
         return view('frontend.pages.flash-sale', compact('flashSaleDate', 'flashSaleItems'));
     }
